98331d1734cad063a6fb0ce47f4ca39d596667e7
jramirez09 <jramirez12@luc.edu>
2022-11-19 15:09:12 -0600

    Home and Sign2

---
+++ with-tailwindcss-app/components/Navbar.js
@@ -0,0 +1,37 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+//<a href="https://www.linkpicture.com/view.php?img=LPic63783515de592311680753"><img src="https://www.linkpicture.com/q/ethereum-patient-medical-records.png" type="image"></a>
+
+function Navbar() {
+  return (
+    // <div>Navbar</div>
+    <>
+        <header className='border border-1-0 border-r-0  bg-[#1B2129] border-white/10 h-[9vh] flex items-center justify-center  fixed top-0 left-0 right-0 opacity-100 z-[2000]'>
+
+            <nav className='flex items-center justify-between w-10/12 lg:w-11/12 m-auto'>
+
+            <Link href={`/`} passHref legacyBehavior>
+                <a className='flex items-center'>
+                    <Image src={`https://raw.githubusercontent.com/thirdweb-dev/typescript-sdk/main/logo.svg`} 
+                    alt='thirdweb/logo'
+                    width={45}
+                    height={45}
+                    priority decoding='async'
+                    objectFit='contain'
+                    />
+                </a>
+            </Link>
+
+            <ul className='flex items-center'>
+                <button type='button' className='className="text-lg text-blue-500 ring-2 ring-blue-600 rounded-lg px-5 py-1.5 hover:bg-blue-600 hover:text-slate-300 transition-all active:scale-90 shadow-lg hover:shadow-blue-600 md:text-sm md:px-4 md:py-1" onClick={connectMetaMask}'>Connect Wallet</button>
+            </ul>
+
+            </nav>
+        </header>
+    </>
+  )
+}
+
+export default Navbar
---
+++ with-tailwindcss-app/components/SignUp.js
@@ -0,0 +1,61 @@
+//rfc snippet to create functional component
+import React from 'react'
+
+export default function SignUp() {
+
+    const signUpFunc = async (event)=>{
+        event.preventDefault();
+        const name = event.target.name.value;
+        const res = await fetch('/api/contact', {
+            body: JSON.stringify({
+              name: name,
+            }),
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            method: 'POST',
+          });
+          const result = await res.json();
+          alert(`Is this your full name: ${result.name}`);
+
+        // const res = await fetch(`https://api.agify.io/?name=${name}`)
+        // alert(`Welcome User ${event.target.name.value}?`);
+    }
+
+    return (
+        <div className="flex items-center justify-center h-[75vh] w-10/12 m-auto lg:w-11/12">
+            <div className="grid items-center justify-items-center text-center max-w-3xl">
+                <h1 className="text-4xl md:text-4xl sm:text-4xl font-bold text-slate-300 underline">Sign Up Please</h1>
+
+                <div>
+                    <form action="/send-data-here" method="post" onSubmit={signUpFunc}>
+                        {/* <label for="roll">Roll Number</label>
+                        <input
+                            type="text"
+                            id="roll"
+                            name="roll"
+                            required
+                            minlength="10"
+                            maxlength="20"
+                        /> */}
+                        <label for="name">Name:</label>
+                        <input type="text" id="name" name="name" required />
+
+                        <label for="name">About:</label>
+                        <input type="text" id="name" name="name" required />
+
+                        <label for="name">Date of Birth:</label>
+                        <input type="text" id="name" name="name" required />
+
+                        <label for="name">Account Type:</label>
+                        <input type="text" id="name" name="name" required />
+
+                        <button type="submit" className="px-4 py-2 font-bold text-white bg-blue-500 rounded-full hover:bg-blue-700">Submit</button>
+
+                    </form>
+                </div>
+            </div>
+
+        </div>
+    )
+}
