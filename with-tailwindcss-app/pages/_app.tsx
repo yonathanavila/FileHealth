4bcae73402e37b9b92fbc4a6301e43bac6a5db62
Yonathan Cruz <50575930+yonathanavila@users.noreply.github.com>
2022-11-20 11:42:22 -0500

    Readme v1 (#8)
    * Add FileHealth main contract and NFT contract
    
    * Fix validation
    
    * Add deployment configuration
    
    * Home and SignUp
    
    * Home and Sign2
    
    * wagmi connect
    
    * doctor page
    
    * adding web3 storage
    
    * web3storage implemented
    
    * patient medical records page
    
    * Fix hydration issues
    
    * Fix: _safeMint to _mint
    
    * Add SPA index and patients
    
    * Add readme and fix isconnected account show the form
    
    * Add logo on nacbar
    
    * Add env example
    
    * Fix deploy bugs
    
    * Fix bugs and update readme
    
    Co-authored-by: jramirez09 <jramirez12@luc.edu>

--- with-tailwindcss-app/pages/_app.tsx
+++ with-tailwindcss-app/pages/_app.tsx
@@ -10,12 +10,7 @@ import {
 
 import { alchemyProvider } from 'wagmi/providers/alchemy'
 import { publicProvider } from 'wagmi/providers/public'
-
-import { CoinbaseWalletConnector } from 'wagmi/connectors/coinbaseWallet'
-import { InjectedConnector } from 'wagmi/connectors/injected'
 import { MetaMaskConnector } from 'wagmi/connectors/metaMask'
-import { WalletConnectConnector } from 'wagmi/connectors/walletConnect'
-import { AuthProvider } from '../context/AuthContext'
 import InternalLayout from '../components/Layouts/InternalLayout'
 
 // Configure chains & providers with the Alchemy provider.
@@ -29,26 +24,7 @@ const { chains, provider, webSocketProvider } = configureChains(defaultChains, [
 const client = createClient({
   autoConnect: true,
   connectors: [
-    new MetaMaskConnector({ chains }),
-    // new CoinbaseWalletConnector({
-    //   chains,
-    //   options: {
-    //     appName: 'wagmi',
-    //   },
-    // }),
-    // new WalletConnectConnector({
-    //   chains,
-    //   options: {
-    //     qrcode: true,
-    //   },
-    // }),
-    // new InjectedConnector({
-    //   chains,
-    //   options: {
-    //     name: 'Injected',
-    //     shimDisconnect: true,
-    //   },
-    // }),
+    new MetaMaskConnector({ chains })
   ],
   provider,
   webSocketProvider,
